604a9a24050020e5aed1be46a1473a3bb34536f4
William Hu <william.hqs@gmail.com>
2014-03-16 11:16:47 +0800

    Finish layout and routes

--- Gemfile
+++ Gemfile
@@ -24,6 +24,10 @@ gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
 
+
+gem 'bootstrap-sass', '2.3.2.0'
+
+
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 1.2'
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -13,30 +13,22 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  subject{page}
   describe "Home page" do
+      before { visit root_path }
 
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('application')
+      it { should have_content('Sample App') }
+      it { should have_title(full_title('')) }
     end
-    # it "should have the right title" do
-#       visit '/static_pages/home'
-#       expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
-#     end
-    it "should not have a custom page title" do
-          visit '/static_pages/home'
-          expect(page).not_to have_title('| Home')
-    end
-  end
   
   describe "Help page" do
 
       it "should have the content 'Help'" do
-        visit '/static_pages/help'
+        visit  help_path
         expect(page).to have_content('help')
       end
       it "should have the right title" do
-        visit '/static_pages/help'
+        visit help_path
         expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
       end
     end
@@ -44,15 +36,25 @@ describe "Static pages" do
     describe "About page" do
 
         it "should have the content 'About Us'" do
-          visit '/static_pages/about'
+          visit about_path
           expect(page).to have_content('About Us')
         end
         it "should have the right title" do
-          visit '/static_pages/about'
+          visit about_path
           expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
         end
     end
 
+    describe  "Contact page" do
+      it "should have the content 'Contact'" do
+        visit contact_path
+        expect(page).to have_content('Contact')
+      end
+      it "should have the title 'Contact'" do
+        visit contact_path
+        expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+      end
+    end
     
 
 end
